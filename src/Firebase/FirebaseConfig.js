b72f305cb9cd0b98e01f32564848d2814798b8b4
jscalderon65 <jhonnseca0599@gmail.com>
2021-10-14 16:21:04 -0500

    Adding dotenv configurations

--- src/Firebase/FirebaseConfig.js
+++ src/Firebase/FirebaseConfig.js
@@ -1,16 +1,16 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
-
+import env from "react-dotenv";
 firebase.initializeApp({
-  apiKey: "AIzaSyD8FvyJKPX-VqQgEDx_mxchzMShfoFDAbg",
-  authDomain: "portfolio-1f2e9.firebaseapp.com",
-  databaseURL: "https://portfolio-1f2e9-default-rtdb.firebaseio.com",
-  projectId: "portfolio-1f2e9",
-  storageBucket: "portfolio-1f2e9.appspot.com",
-  messagingSenderId: "962989852779",
-  appId: "1:962989852779:web:1de67b8015b0d0361f092e",
-  measurementId: "G-P68W5LFXBG",
+  apiKey: env.API_KEY,
+  authDomain: env.AUTH_DOMAIN,
+  databaseURL: env.DATABASE_URL,
+  projectId: env.PROJECT_ID,
+  storageBucket: env.STORAGE_BUCKET,
+  messagingSenderId: env.MESSAGING_SENDER_ID,
+  appId: env.APP_ID,
+  measurementId: env.MEASUREMENT_ID,
 });
 
 const db = firebase.firestore();
